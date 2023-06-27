cd67c6327e662fce85eac398c076b66ecee7cf82
Cesar Mendoza <52425885+mendoce24@users.noreply.github.com>
2023-06-27 08:54:20 +0200

    modification of dictionary to list

--- test.py
+++ test.py
@@ -7,12 +7,11 @@ def get_properties(url, session):
    req = session.get(url)
    soup = BeautifulSoup(req.content, "html")
    articles = soup.find_all('article')
-   infor_property = dict()
+   infor_property = []
    
    for article in articles:
       url_property = article.find('a')['href']
-      infor_property['url'] = url_property
-      infor_property['code'] = url_property.split('/')[-1]
+      infor_property.append([url_property.split('/')[-1], url_property])
 
    print(infor_property)
 
