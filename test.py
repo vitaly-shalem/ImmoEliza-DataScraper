e95b6d7da33e3adce68a71b6cb4f9880fdbf7226
fel <felicien.dehertogh@protonmail.com>
2023-06-29 12:23:03 +0200

    Add property_type argument to id scraper

--- test.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-print(Path.cwd())
-
-file_name = Path("id.txt")
-
-file_path = Path.cwd() / "data" / file_name
-
-print(file_path)
--- utils/get_property_ids.py
+++ utils/get_property_ids.py
@@ -5,36 +5,49 @@ import itertools
 from pathlib import Path
 
 
-def get_ids_from_page(page, session):
+def get_ids_from_page(page, session, property_type):
     ids = []
-    url_lists = f"https://www.immoweb.be/en/search-results/house-and-apartment/for-sale?countries=BE&page={page}&orderBy=newest"
+    url_lists = "https://www.immoweb.be/en/search-results/%s/for-sale?countries=BE&page=%s&orderBy=newest" % (property_type, page)
     r = session.get(url_lists)
     ids = [listing['id'] for listing in r.json()["results"]]
     return ids
 
 
-def get_ids(pages, max_workers):
+def get_ids(pages, property_type):
     with requests.Session() as session:
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            start = time.time()
-            result = list(executor.map(lambda page: get_ids_from_page(page, session), range(1, pages+1)))
+        with ThreadPoolExecutor(max_workers=30) as executor:
+            result = list(executor.map(lambda page: get_ids_from_page(page, session, property_type), range(1, pages+1)))
             flattened_result = list(itertools.chain.from_iterable(result))
-            end = time.time()
             print(f"Number of ids: {len(flattened_result)}")
-            print("Time Taken: {:.6f}s".format(end-start))
             return flattened_result
 
 
-def save_to_txt(ids):
-    file_name = Path("id.txt")
+def save_to_txt(ids, property_type):
+    file_name = Path(f"{property_type}_listing_ids.txt")
     file_path = Path.cwd() / "data" / file_name
     with open(file_path, 'w') as f:
         for id in ids:
             f.write('%s\n' % id)
 
 
+def id_scraper(pages, property_type):
+    """
+    Scrape ids and save them into a text file.
+
+    @param pages: number of pages to scrape.
+    @param property_type: type of property to scrape.
+    """
+    if property_type == "house" or property_type == "apartment":
+        start = time.time()
+        ids = get_ids(pages, property_type)
+        save_to_txt(ids, property_type)
+        end = time.time()
+        print("Time taken to scrape ids: {:.6f}s".format(end-start))
+    else:
+        print(
+            "Invalid argument: please use 'house' or 'apartment' as second argument.")
+        return
+
+
 if __name__ == "__main__":
-    start = time.time()
-    save_to_txt(get_ids(5, 10))
-    end = time.time()
-    print("Total Time Taken: {:.6f}s".format(end-start))
+    id_scraper(10, "apartment")
